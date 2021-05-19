fa95b225b4ba41efd405223535da26817a616c81
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-19 15:41:04 +0200

    structure

--- index.php
+++ index.php
@@ -4,11 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Style -->
+    <link rel="stylesheet" href="./style/style.css">
+    <!-- / Style -->
     <title>Google Faq</title>
 </head>
 <body>
-    
+    <!--Header -->
+    <header>
+    <div class="container">
+        <div class="upper-header flex">
+            <img src="./style/img/logo.png" alt="Google logo">
+            <div>Privacy e termini</div>
+            <div class="filler"></div>
+            <div class="icon">hello</div>
+            <div class="image">hello</div>
+        </div>
+        <div class="lower-header flex">
+            <div class="menu"><a href="#">Introduzione</a></div>
+            <div class="menu"><a href="#">Norme sulla privacy</a></div>
+            <div class="menu"><a href="#">Termini di servizio</a></div>
+            <div class="menu"><a href="#">Tecnologie</a></div>
+            <div class="menu"><a href="#">Domande frequenti</a></div>
+        </div>
+    </div>
+    </header>
 
-    
+    <!-- / Header -->
+
+    <!--Main -->
+
+    <main>
+        <div class="second-container"> <!--PHP SECTION HERE -->
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    </main>
+
+    <!--/Main -->
+
 </body>
 </html>
