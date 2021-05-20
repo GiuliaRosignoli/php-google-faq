b5e08dcd9d796695f1b376d84a622e2a52360c1d
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-20 08:49:38 +0200

    .

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
     <!--FontAwesome cdn -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <!--/FontAwesome cdn -->
-    <title>Google Faq</title>
+    <title>Domande frequenti</title>
 </head>
 <body>
     <!--Header -->
